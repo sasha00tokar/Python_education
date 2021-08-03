31d3088480ebc60f94e676c417107ee1f3b5dcec
sanya <tokarsasha84@gmail.com>
2021-08-03 23:58:58 +0300

    right task

--- exercise/Shop.py
+++ exercise/Shop.py
@@ -1,57 +1,58 @@
+store = {
+    'banana': {'price': 10, 'discount_price': 7, 'discount_count': 3},
+    'mango': {'price': 15, 'discount_price': 12, 'discount_count': 3},
+    'apple': {'price': 20, 'discount_price': 17, 'discount_count': 3}
+}
+
+
 class Item:
-    def __init__(self, n, c, p):
+    def __init__(self, n, c):
         self.name = n
         self.count = c
-        self.price = p
+
     def __str__(self):
         return str(self.name)
+
+
 class Bag:
-    shopping_list = {}
-    # def __str__(self):
-    #     return self.name
+    def __init__(self):
+        self.shopping_list = {}
+
+    def bulk_discount_price(self):
+        for key, value in self.shopping_list.items():
+            value['price'] = (value['count'] - (value['count'] % store[key]['discount_count'])) * \
+                             store[key]['discount_price'] + (value['count'] % store[key]['discount_count']) * \
+                             store[key]['price']
+
     def show_bag(self):
-        for key in self.shopping_list:
-            # print(self.shopping_list[key]['count'])
-            print('{} : count {}, price {}'.format(key,
-                  self.shopping_list[key]['count'], self.shopping_list[key]['price']))
+        self.bulk_discount_price()
+        return self.shopping_list
+
     def add_bag(self, item):
         if item.name not in self.shopping_list:
-            self.shopping_list[item.name] = {'count':item.count, 'price':item.price}
+            self.shopping_list[item.name] = {'count': item.count}
         else:
-            self.shopping_list[item.name]['count'] += 1
-            self.shopping_list[item.name]['price'] += item.price
+            self.shopping_list[item.name]['count'] += item.count
+
     def del_from_bag(self, item):
         if item.name in self.shopping_list:
             self.shopping_list.pop(item.name)
+            return True
         else:
-            print('Товара нету в корзине!')
-    def change_count(self, item):
-        n = int(input('Введіть к-сть одиниць товару:'))
-        self.shopping_list[item.name]['count'] = n
-        self.shopping_list[item.name]['price'] = item.price * n
+            return False
+
     def sum_price(self):
-        sum = 0
-        for el in self.shopping_list:
-            sum += self.shopping_list[el]['price']
-        return print('Сумма всіх товарів в корзині: {}'.format(sum))
+        return sum(value['price'] for key, value in self.shopping_list.items())
 
-p1 = Item('banana', 1, 10)
-p2 = Item('banana', 1, 10)
-p3 = Item('mango', 1, 20)
-c = Bag()
-c.add_bag(p1)
-c.add_bag(p2)
-c.add_bag(p3)
-c.show_bag()
-c.sum_price()
-
-
-
-
-
-
-
-
-    # def addToBag(self, n, c, p):
-    #     self.shopping_list.append(Item(n, c, p))
-
+# p1 = Item('banana', 1)
+# p2 = Item('banana', 3)
+# p3 = Item('mango', 1)
+# p4 = Item('mango', 2)
+# c = Bag()
+# c.add_bag(p1)
+# c.add_bag(p2)
+# c.add_bag(p3)
+# c.add_bag(p4)
+# print(c.show_bag())
+# print(c.sum_price())
+# #print(c.show_bag())
