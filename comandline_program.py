d78d3a75eeec900935d0a7adeeb4cceae1744294
sanya <tokarsasha84@gmail.com>
2021-09-02 01:12:09 +0300

    implementation of the argument "duplicates"

--- comandline_program.py
+++ comandline_program.py
@@ -1,5 +1,30 @@
+import os
 import argparse
 parser = argparse.ArgumentParser()
+
+
+def duplicates_arg(way, optional=None):
+    name_files = [f for f in os.listdir(way) if f.endswith('.py')]
+    if optional is None:
+        for name in name_files:
+            print(name)
+    else:
+        with open(optional, "w") as file_write:
+            for name in name_files:
+                file_write.write(name + "\n")
+
+
+def large_arg(way, size):
+    pass
+
+
+def images_arg(way):
+    pass
+
+
+def old_arg(way):
+    pass
+
 
 parser.add_argument("-o", type=str, help="In which file the results will be written")
 parser.add_argument("-size", type=int, help="Size file" )
@@ -8,8 +33,9 @@ parser.add_argument(choices=["duplicates", "large", "images", "old"],
                                           "large - search files larger than the specified size,"
                                           "images - searches for image files,"
                                           "old - searches for files older than one year")
-
 parser.add_argument("way", help="File path")
 
-args = parser.parse_args()
-print(args)
+if parser.parse_args().argument == "duplicates":
+    duplicates_arg(parser.parse_args().way, parser.parse_args().o)
+else:
+    print("NOT")
