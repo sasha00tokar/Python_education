758c7b6a5c9fa02a8294f783446909f048af907e
sanya <tokarsasha84@gmail.com>
2021-09-09 23:00:41 +0300

    commandline.py file optimization

--- commandline-project/commandline/commandline.py
+++ commandline-project/commandline/commandline.py
@@ -12,48 +12,33 @@ def write_to_file(file, data):
         file_write.write('\n'.join(data))
 
 
-def duplicates_arg(way, name_file=None):
+def identical_files(way):
     files_list = [f for f in os.listdir(way) if f.endswith('.py')]
-    if name_file is None:
-        for name in files_list:
-            return name
-    else:
-        write_to_file(name_file, files_list)
+    return [str(name) for name in files_list]
 
 
-def large_arg(way, size, name_file=None):
+def files_are_larger(way, size):
     files_list = [f for f in os.listdir(way)]
-    for name in files_list:
-        if os.stat(name).st_size > size:
-            if name_file is None:
-                return name
-            else:
-                write_to_file(name_file, files_list)
-        else:
-            continue
+    return [str(name) for name in files_list if os.stat(way + '/' + name).st_size > size]
 
 
-def images_arg(way, name_file=None):
+def image_files(way):
     files_list = [f for f in os.listdir(way) if f.endswith('.jpeg')]
-    if name_file is None:
-        for name in files_list:
-            return name
-    else:
-        write_to_file(name_file, files_list)
+    return [str(name) for name in files_list]
 
 
-def old_arg(way, name_file=None):
+def files_older_than_a_year(way):
     files_list = [f for f in os.listdir(way)]
     one_year_ago = datetime.now() - relativedelta(years=1)
-    for name in files_list:
-        file_time = datetime.fromtimestamp(os.path.getmtime(name))
-        if file_time < one_year_ago:
-            if name_file is None:
-                print(name)
-            else:
-                write_to_file(name_file, files_list)
-        else:
-            continue
+    return [str(name) for name in files_list
+            if datetime.fromtimestamp(os.path.getmtime(way + '/' + name)) < one_year_ago]
+
+
+def output_or_write_to_file(name_function, name_file):
+    if name_file is not None:
+        write_to_file(name_file, name_function)
+    else:
+        name_function
 
 
 def main():
@@ -67,12 +52,13 @@ def main():
     parser.add_argument("way", help="File path")
 
     if parser.parse_args().argument == "duplicates":
-        duplicates_arg(parser.parse_args().way, parser.parse_args().o)
+        output_or_write_to_file(identical_files(parser.parse_args().way), parser.parse_args().o)
     elif parser.parse_args().argument == "large":
-        large_arg(parser.parse_args().way, parser.parse_args().size, parser.parse_args().o)
+        output_or_write_to_file(files_are_larger(parser.parse_args().way, parser.parse_args().size),
+                                parser.parse_args().o)
     elif parser.parse_args().argument == "images":
-        images_arg(parser.parse_args().way, parser.parse_args().o)
+        output_or_write_to_file(image_files(parser.parse_args().way), parser.parse_args().o)
     elif parser.parse_args().argument == "old":
-        old_arg(parser.parse_args().way, parser.parse_args().o)
+        output_or_write_to_file(files_older_than_a_year(parser.parse_args().way), parser.parse_args().o)
     else:
         print("NOT")
