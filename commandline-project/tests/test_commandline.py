4990e31b486421ec0c296f9655c8db9317847d98
sanya <tokarsasha84@gmail.com>
2021-10-01 22:54:07 +0300

    test for commandline.py with mock

--- commandline-project/tests/test_commandline.py
+++ commandline-project/tests/test_commandline.py
@@ -1,56 +1,26 @@
-import os
-import os.path
-import time
-import shutil
+from attrdict import AttrDict
+import pytest
+
 from commandline.commandline import identical_files, files_are_larger, files_older_than_a_year, image_files
 
 
-def make_dir(file_list, directory):
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-    for name_file in file_list:
-        open(os.path.join(directory, name_file), "a")
+def test_identical_files(mocker):
+    mocker.patch("os.listdir", return_value=["file_2.py", "file_1.py", "file_3"])
+    assert sorted(identical_files("foo/folder")) == sorted(["file_1.py", "file_2.py"])
 
 
-def test_commandline():
-    directory = "folder"
-    good_ret = ["file_2.py", "file_1.py"]
-    file_list = ["file_1.py", "file_2.py", "file_3"]
-    make_dir(file_list, directory)
-    result = identical_files(directory)
-    assert sorted(result) == sorted(good_ret)
+def test_image_files(mocker):
+    mocker.patch("os.listdir", return_value=["file_1.py", "file_3.jpeg", "file_2.py", "file_3", "file_4.jpeg"])
+    assert sorted(image_files("foo/folder")) == sorted(["file_3.jpeg", "file_4.jpeg"])
 
 
-def test_image_files():
-    directory = "folder"
-    good_ret = ["file_3.jpeg", "file_4.jpeg"]
-    file_list = ["file_1.py", "file_3.jpeg", "file_2.py", "file_3", "file_4.jpeg"]
-    make_dir(file_list, directory)
-    result = image_files(directory)
-    assert sorted(result) == sorted(good_ret)
+def test_files_are_larger(mocker):
+    mocker.patch("os.listdir", return_value=["file_1.py", "file_3.jpeg", "file_2.py"])
+    mocker.patch("os.stat", return_value=AttrDict({"st_size": 5}))
+    assert sorted(files_are_larger("foo/folder", 4)) == sorted(["file_1.py", "file_3.jpeg", "file_2.py"])
 
 
-def test_files_are_larger():
-    size = 4
-    directory = "folder"
-    good_ret = ["file_2.py"]
-    file_list = ["file_1.py", "file_3.jpeg", "file_2.py", "file_3", "file_4.jpeg"]
-    make_dir(file_list, directory)
-    with open(directory + "/file_2.py", "w") as f:
-        f.write('sanya')
-    result = files_are_larger(directory, size)
-    assert sorted(result) == sorted(good_ret)
-
-
-def test_files_older_than_a_year():
-    value = 1
-    directory = "folder"
-    good_ret = ["file_1.py", "file_3.jpeg", "file_2.py", "file_3", "file_4.jpeg"]
-    file_list = ["file_1.py", "file_3.jpeg", "file_2.py", "file_3", "file_4.jpeg"]
-    make_dir(file_list, directory)
-    time.sleep(1)
-    result = files_older_than_a_year(directory, value)
-    shutil.rmtree(directory)
-    assert sorted(result) == sorted(good_ret)
-
-
+def test_files_older_than_a_year(mocker):
+    mocker.patch("os.listdir", return_value=["file_1.py", "file_3.jpeg", "file_2.py"])
+    mocker.patch("os.path.getmtime", return_value=1500000000)
+    assert sorted(files_older_than_a_year("foo/folder", 1)) == sorted(["file_1.py", "file_3.jpeg", "file_2.py"])
